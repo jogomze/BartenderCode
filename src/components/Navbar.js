0bd39d5608fee38cb2f029e8c34a45aac89f159a
Josegabrielgomezp <Code1801gp@gmail.com>
2023-01-27 16:08:26 +0000

    logout hecho

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,48 +1,46 @@
 import React, { useState } from 'react';
-import { Link, Redirect } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [shouldRedirect, setShouldRedirect] = useState(false);
-  const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
-  const handleLogout = () => {
-    setIsLoggedIn(false);
-    setShouldRedirect(true);
-  }
-  const handleLogin = () => setIsLoggedIn(true);
+const navigate = useNavigate();
+const [dropdownOpen, setDropdownOpen] = useState(false);
+const [isLoggedIn, setIsLoggedIn] = useState(false);
+const toggleDropdown = () => setDropdownOpen(!dropdownOpen);
+const handleLogout = () => {
+setIsLoggedIn(false);
+navigate('/home');
+}
+const handleLogin = () => setIsLoggedIn(true);
 
-  return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <Link to="/"><h2 className="logo">Bartender Code</h2></Link>
-          <ul className="nav-links">
-              <li><Link to="/">Home</Link></li>
-              <li><Link to="/about">About</Link></li>
-              
-              {isLoggedIn ? (
-              <>
-                <li>
-                  <div onClick={toggleDropdown}>
-                    <a className="dropdown-username">Username</a>
-                    {dropdownOpen && (
-                      <ul className="dropdown">
-                        <li><Link to="/logout" onClick={handleLogout}>Logout</Link></li>
-                      </ul>
-                    )}
-                  </div>
-                </li>
-              </>
-              ) 
-              : 
-              (
-              <li><Link to="/login" onClick={handleLogin}>Login</Link></li>
-              )}
-            </ul>
-            {shouldRedirect && <Redirect to="/home" />}
+return (
+  <nav className="navbar">
+    <div className="nav-center">
+    <Link to="/"><h2 className="logo">Bartender Code</h2></Link>
+      <ul className="nav-links">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        {isLoggedIn ? (
+          <>
+            <li>
+              <div onClick={toggleDropdown}>
+                <a className="dropdown-username">Username</a>
+                {dropdownOpen && (
+                  <ul className="dropdown">
+                    <li><Link to="/logout" onClick={handleLogout}>Logout</Link></li>
+                  </ul>
+                )}
+              </div>
+            </li>
+          </>
+          ) 
+          : 
+          (
+          <li><Link to="/login" onClick={handleLogin}>Login</Link></li>
+          )}
+        </ul>
       </div>
-    </nav>
-  )
+</nav>
+)
 }
 
 export default Navbar;
--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,15 +1,20 @@
-import React from 'react'
-import {Link} from "react-router-dom";
+import React, { useEffect } from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
 const Error = () => {
-  return (
-    <section className="section error-page">
-      <div className="error-container">
-        <h1>oops! it's a dead end!</h1>
-        <Link to="/" className="btn btn-primary">Back Home</Link>
-      </div>
-    </section>
-  )
+const navigate = useNavigate();
+
+useEffect(() => {
+navigate("/");
+}, [navigate]);
+
+return (
+  <section className="section error-page">
+    <div className="error-container">
+        <Link to="/" ></Link>
+    </div>
+  </section>
+)
 }
 
 export default Error
