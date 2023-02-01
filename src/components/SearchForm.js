9fbe6f306a60195c1f897bdd7678d94abd53dc67
Jose Gomez <jose.gomezpereira001@mymdc.net>
2023-02-01 01:44:59 +0000

    change about us and the size of the letters

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -23,7 +23,7 @@ const SearchForm = () => {
     <section className="section search">
       <form className="search-form" onSubmit={handleSubmit}>
         <div className="form-control">
-            <label htmlFor="name">search your favorite cocktail</label>
+            <h2><label htmlFor="name">search your favorite cocktail</label></h2>
             <input type="text" id="name" ref={searchValue} onChange={searchCocktail}></input>
         </div>
       </form>
--- src/pages/About.js
+++ src/pages/About.js
@@ -4,9 +4,13 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">about us</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias quod eum explicabo officia
-       aliquid id minus eos natus iure exercitationem corporis, iste totam labore nisi iusto doloribus 
-       similique inventore suscipit sunt consequuntur harum deserunt laborum esse eaque? Fugit, repudiandae officiis.</p>
+      <p>Bartender Code is an JavaScript app that can find cocktails using an API and has a feature where you can see random cocktails by pressing a button would use several technologies to accomplish this. The app would use HTML, CSS and JavaScript to create the user interface and layout of the app. It would use React, a JavaScript library, to build the app's structure and handle the dynamic updates of the page based on user interactions.
+
+The app would use an API to retrieve information on cocktails, such as the ingredients and instructions for making them. This API would likely be called using JavaScript code within the React components of the app.
+
+To handle the feature of seeing random cocktails by pressing a button, the app would likely use JavaScript to make a call to the API, passing in a request for a random cocktail. The API would then return a random cocktail from its database, which would be displayed on the page through the use of React components.
+
+Finally, for user authentication, the app would use Firebase from Google to handle user registration and login. This would allow users to create an account and log in to the app.</p>
     </section>
   )
 }
